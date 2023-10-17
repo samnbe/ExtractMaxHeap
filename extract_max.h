4483ad6f57ea9fe04213d0c1c5f22857c8ee0a35
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2023-10-16 22:18:14 -0400

    Update extract_max.h
    added if size == 0 result
    fixed improper tabs

--- extract_max.h
+++ extract_max.h
@@ -2,25 +2,28 @@
 #include <iostream>
 
 void percolateDown(int arr[], int size, int parent) {
-	int left = (2*parent) + 1;
+    int left = (2*parent) + 1;
     int right = (2*parent) + 2;
-	int maxIndex = parent;
+    int maxIndex = parent;
 
     if (right < size && arr[right] > arr[maxIndex])
-	    maxIndex = right;
+	maxIndex = right;
     if (left < size && arr[left] > arr[maxIndex])
-	    maxIndex = left;
+	maxIndex = left;
     if (maxIndex != parent) {
-	    std::swap(arr[maxIndex], arr[parent]);
-	    percolateDown(arr, size, maxIndex);
-	}
+	std::swap(arr[maxIndex], arr[parent]);
+	percolateDown(arr, size, maxIndex);
+    }
 }
 
 int* extractMax(int arr[], int size) {
+    if (size == 0){
+	return nullptr;
+    }
     arr[0] = arr[size - 1];
     size--;
     percolateDown(arr, size, 0);
     
-	return arr;
+    return arr;
 }
 
