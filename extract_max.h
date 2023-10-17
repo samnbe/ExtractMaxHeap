6c4fd77ec429aebae47c065a59a739aea0366db5
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2023-10-16 22:14:26 -0400

    Add files via upload

---
+++ extract_max.h
@@ -0,0 +1,26 @@
+//Worked with Trevor Gross
+#include <iostream>
+
+void percolateDown(int arr[], int size, int parent) {
+	int left = (2*parent) + 1;
+    int right = (2*parent) + 2;
+	int maxIndex = parent;
+
+    if (right < size && arr[right] > arr[maxIndex])
+	    maxIndex = right;
+    if (left < size && arr[left] > arr[maxIndex])
+	    maxIndex = left;
+    if (maxIndex != parent) {
+	    std::swap(arr[maxIndex], arr[parent]);
+	    percolateDown(arr, size, maxIndex);
+	}
+}
+
+int* extractMax(int arr[], int size) {
+    arr[0] = arr[size - 1];
+    size--;
+    percolateDown(arr, size, 0);
+    
+	return arr;
+}
+
